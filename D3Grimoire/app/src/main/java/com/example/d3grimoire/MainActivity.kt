ec9fb6f3583af76a1a481f57f5f6a4bcb19b9446
SergiNaranjo <sergi.naranjo@enti.cat>
2025-12-01 11:01:29 +0100

    Some UIs created

--- D3Grimoire/app/src/main/java/com/example/d3grimoire/MainActivity.kt
+++ D3Grimoire/app/src/main/java/com/example/d3grimoire/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.d3grimoire
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_screen)
     }
 }
