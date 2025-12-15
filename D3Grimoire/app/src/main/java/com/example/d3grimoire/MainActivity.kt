18cb7731003bb111b4df6f63d45a3c4cc67a6b70
SergiNaranjo <sergi.naranjo@enti.cat>
2025-12-15 09:57:48 +0100

    Fixing build

--- D3Grimoire/app/src/main/java/com/example/d3grimoire/MainActivity.kt
+++ D3Grimoire/app/src/main/java/com/example/d3grimoire/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.d3grimoire
 
 import android.os.Bundle
+import android.view.animation.LinearInterpolator
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+
+        val image = findViewById<ImageView>(R.id.imageView3)
+
+        val rotate = RotateAnimation(
+            0f,
+            360f,
+            RotateAnimation.RELATIVE_TO_SELF,
+            0.5f,
+            RotateAnimation.RELATIVE_TO_SELF,
+            0.5f
+        )
+
+        rotate.duration = 2000
+        rotate.repeatCount = RotateAnimation.INFINITE
+        rotate.interpolator = LinearInterpolator()
+
+        image.startAnimation(rotate)
     }
 }
