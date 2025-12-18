959f7e92d11b8c953f82e352bc15eca978447ef3
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-12-18 23:34:26 +0100

    Created NavBar activity and functional fragment change

--- D3Grimoire/app/src/main/java/com/example/d3grimoire/ProfileActivity.kt
+++ D3Grimoire/app/src/main/java/com/example/d3grimoire/ProfileActivity.kt
@@ -1,25 +1,26 @@
 package com.example.d3grimoire
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 
-class ProfileActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class ProfileActivity : Fragment(R.layout.profile_screen) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.profile_screen)
 
         var bundle : Bundle = bundleOf(
             "name" to "Name",
             "class" to "Barbarian",
             "level" to 5
         );
-        supportFragmentManager.commit {
+        childFragmentManager.commit {
             setReorderingAllowed(true);
             add<ProfileHero>(R.id.hero_1, args = bundle);
         }
@@ -29,7 +30,7 @@ class ProfileActivity : AppCompatActivity() {
             "class" to "Barbarian",
             "level" to 5
         );
-        supportFragmentManager.commit {
+        childFragmentManager.commit {
             setReorderingAllowed(true);
             add<ProfileHero>(R.id.hero_2, args = bundle);
         }
@@ -39,7 +40,7 @@ class ProfileActivity : AppCompatActivity() {
             "class" to "Barbarian",
             "level" to 5
         );
-        supportFragmentManager.commit {
+        childFragmentManager.commit {
             setReorderingAllowed(true);
             add<ProfileHero>(R.id.hero_3, args = bundle);
         }
