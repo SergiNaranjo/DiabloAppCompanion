c4e12b22fe52daf7ebd1c63d0098983412e4971b
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-12-18 13:35:15 +0100

    Moved posts to a Fragment

---
+++ D3Grimoire/app/src/main/java/com/example/d3grimoire/CommunityPost.kt
@@ -0,0 +1,29 @@
+package com.example.d3grimoire
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class CommunityPost : Fragment(R.layout.activity_community_post) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //Load card title
+        val cardTitle : String? = requireArguments().getString("title");
+        var textView : TextView = view.findViewById<TextView>(R.id.cardTitle);
+        textView.text = cardTitle;
+
+        //Load description
+        val cardDescription : String? = requireArguments().getString("desc");
+        textView = view.findViewById<TextView>(R.id.cardDescription);
+        textView.text = cardDescription;
+
+        //Load description
+        val cardAuthor : String? = requireArguments().getString("author");
+        textView = view.findViewById<TextView>(R.id.cardPosted);
+        textView.text = cardAuthor;
+    }
+}
--- D3Grimoire/app/src/main/java/com/example/d3grimoire/MainActivity.kt
+++ D3Grimoire/app/src/main/java/com/example/d3grimoire/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.d3grimoire
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
