04b084da2e010edf627dfd20afe864aa3ce3ce61
DrRon <DoctorRon64@users.noreply.github.com>
2025-12-19 00:01:40 +0100

    [ADDED] TokenManager + BlizzardAuthInstance + DiabloInstance [ADDED] first activities

--- D3Grimoire/app/src/main/java/API/BlizzardAuthApi.kt
+++ D3Grimoire/app/src/main/java/API/BlizzardAuthApi.kt
@@ -6,9 +6,8 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface BlizzardAuthApi {
-
-    @POST("token")
     @FormUrlEncoded
+    @POST("token")
     fun getAccessToken(
         @Field("grant_type") grantType: String = "client_credentials"
     ): Call<AuthResponse>
--- D3Grimoire/app/src/main/java/com/example/d3grimoire/MainActivity.kt
+++ D3Grimoire/app/src/main/java/com/example/d3grimoire/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.d3grimoire
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
