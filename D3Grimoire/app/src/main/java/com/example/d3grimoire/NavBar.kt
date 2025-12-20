d4cf397e72d6c1ab7bb38706e82d48e480f32859
SergiNaranjo <sergi.naranjo@enti.cat>
2025-12-20 18:09:56 +0100

    changes

--- D3Grimoire/app/src/main/java/com/example/d3grimoire/NavBar.kt
+++ D3Grimoire/app/src/main/java/com/example/d3grimoire/NavBar.kt
@@ -2,15 +2,26 @@ package com.example.d3grimoire
 
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import android.view.View
+import com.example.d3grimoire.posts.community.CommunityScreen
+import com.example.d3grimoire.posts.news.NewsScreen
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
 
 class NavBar : AppCompatActivity() {
-
+    enum class PostScreen {
+        NEWS, COMMUNITY
+    }
     private lateinit var btnNews: ImageButton
     private lateinit var btnInfo: ImageButton
     private lateinit var btnProfile: ImageButton
+
+    private lateinit var floatingButtons: LinearLayout;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +33,54 @@ class NavBar : AppCompatActivity() {
 
         setupListeners()
 
-        // Fragment inicial
         selectTab(btnNews)
+
+        floatingButtons = findViewById<LinearLayout>(R.id.floatingButtons);
+
+        val communityScreenButton: ImageButton = findViewById<ImageButton>(R.id.community_screen);
+        val newsScreenButton: ImageButton = findViewById<ImageButton>(R.id.news_screen);
+        communityScreenButton.setOnClickListener {
+            postScreen = PostScreen.COMMUNITY;
+            loadFragment(CommunityScreen());
+            communityScreenButton.setBackgroundResource(R.drawable.circle_bg);
+            newsScreenButton.setBackgroundResource(R.drawable.circle_bg_default);
+        }
+        newsScreenButton.setOnClickListener {
+            postScreen = PostScreen.NEWS;
+            loadFragment(NewsScreen());
+            communityScreenButton.setBackgroundResource(R.drawable.circle_bg_default);
+            newsScreenButton.setBackgroundResource(R.drawable.circle_bg);
+        }
+
+        postScreen = PostScreen.NEWS;
         loadFragment(NewsScreen())
     }
 
     private fun setupListeners() {
         btnNews.setOnClickListener {
             selectTab(btnNews)
-            loadFragment(NewsScreen())
+            when (postScreen) {
+                PostScreen.NEWS -> {
+                    loadFragment(NewsScreen())
+                }
+
+                PostScreen.COMMUNITY -> {
+                    loadFragment(CommunityScreen())
+                }
+            }
+            floatingButtons.visibility = View.VISIBLE;
         }
 
         btnInfo.setOnClickListener {
             selectTab(btnInfo)
             loadFragment(InfoScreen())
+            floatingButtons.visibility = View.GONE;
         }
 
         btnProfile.setOnClickListener {
             selectTab(btnProfile)
             loadFragment(ProfileActivity())
+            floatingButtons.visibility = View.GONE;
         }
     }
 
