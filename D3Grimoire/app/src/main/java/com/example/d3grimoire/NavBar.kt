be2a49b94f9a2075ca7e40ce6c96c9b3546086dc
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-12-20 17:35:03 +0100

    Made CommunityScreen a Fragment and moved floating buttons to NavBar

--- D3Grimoire/app/src/main/java/com/example/d3grimoire/NavBar.kt
+++ D3Grimoire/app/src/main/java/com/example/d3grimoire/NavBar.kt
@@ -1,15 +1,25 @@
 package com.example.d3grimoire
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import com.example.d3grimoire.posts.community.CommunityScreen
 import com.example.d3grimoire.posts.news.NewsScreen
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NavBar : AppCompatActivity() {
 
+    enum class PostScreen {
+        NEWS, COMMUNITY
+    }
+
     private lateinit var bottomNavigationView: BottomNavigationView;
+    private lateinit var floatingButtons: LinearLayout;
+    private lateinit var postScreen: PostScreen;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
@@ -19,6 +29,25 @@ class NavBar : AppCompatActivity() {
         bottomNavigationView.setOnItemSelectedListener { item ->
             handleNavigationItemSelected(item.itemId);
         }
+
+        floatingButtons = findViewById<LinearLayout>(R.id.floatingButtons);
+
+        val communityScreenButton: ImageButton = findViewById<ImageButton>(R.id.community_screen);
+        val newsScreenButton: ImageButton = findViewById<ImageButton>(R.id.news_screen);
+        communityScreenButton.setOnClickListener {
+            postScreen = PostScreen.COMMUNITY;
+            loadFragment(CommunityScreen());
+            communityScreenButton.setBackgroundResource(R.drawable.circle_bg);
+            newsScreenButton.setBackgroundResource(R.drawable.circle_bg_default);
+        }
+        newsScreenButton.setOnClickListener {
+            postScreen = PostScreen.NEWS;
+            loadFragment(NewsScreen());
+            communityScreenButton.setBackgroundResource(R.drawable.circle_bg_default);
+            newsScreenButton.setBackgroundResource(R.drawable.circle_bg);
+        }
+
+        postScreen = PostScreen.NEWS;
 
         loadFragment(NewsScreen());
     }
@@ -33,15 +62,25 @@ class NavBar : AppCompatActivity() {
     private fun handleNavigationItemSelected(itemId: Int): Boolean {
         return when (itemId) {
             R.id.news -> {
-                loadFragment(NewsScreen())
+                when (postScreen) {
+                    PostScreen.NEWS -> {
+                        loadFragment(NewsScreen())
+                    }
+                    PostScreen.COMMUNITY -> {
+                        loadFragment(CommunityScreen())
+                    }
+                }
+                floatingButtons.visibility = View.VISIBLE;
                 true
             }
             R.id.info -> {
                 loadFragment(InfoScreen())
+                floatingButtons.visibility = View.GONE;
                 true
             }
             R.id.profile -> {
                 loadFragment(ProfileActivity())
+                floatingButtons.visibility = View.GONE;
                 true
             }
             else -> false
--- D3Grimoire/app/src/main/java/com/example/d3grimoire/posts/community/CommunityScreen.kt
+++ D3Grimoire/app/src/main/java/com/example/d3grimoire/posts/community/CommunityScreen.kt
@@ -1,19 +1,19 @@
 package com.example.d3grimoire.posts.community
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.d3grimoire.R
 
-class CommunityScreen : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.community_screen)
+class CommunityScreen : Fragment(R.layout.community_screen) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        val imgBtn: ImageButton = findViewById<ImageButton>(R.id.new_post);
+        val imgBtn: ImageButton = view.findViewById<ImageButton>(R.id.new_post);
         imgBtn.setOnClickListener { newPost(); }
 
         var bundle : Bundle = bundleOf(
@@ -21,7 +21,7 @@ class CommunityScreen : AppCompatActivity() {
             "desc" to "A new advanced tutorial has been released...",
             "author" to "Posted by RazR0thy22X"
         );
-        supportFragmentManager.commit {
+        childFragmentManager.commit {
             setReorderingAllowed(true);
             add<CommunityPost>(R.id.fragment_container_view, args = bundle);
         }
@@ -31,7 +31,7 @@ class CommunityScreen : AppCompatActivity() {
             "desc" to "A complete advanced tutorial has been released...",
             "author" to "Posted by EnSparta II"
         );
-        supportFragmentManager.commit {
+        childFragmentManager.commit {
             setReorderingAllowed(true);
             add<CommunityPost>(R.id.post_2, args = bundle);
         }
@@ -41,7 +41,7 @@ class CommunityScreen : AppCompatActivity() {
             "desc" to "A new advanced tutorial has been released...",
             "author" to "Posted by RazR0thy22X"
         );
-        supportFragmentManager.commit {
+        childFragmentManager.commit {
             setReorderingAllowed(true);
             add<CommunityPost>(R.id.post_3, args = bundle);
         }
