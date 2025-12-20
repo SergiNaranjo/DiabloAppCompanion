930fcab055c53ca1949298c1e20c21ecb7b2f777
DrRon <DoctorRon64@users.noreply.github.com>
2025-12-20 18:47:34 +0100

    [ADDED] improved build files

--- D3Grimoire/app/build.gradle.kts
+++ D3Grimoire/app/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.gms.google-services")
-    alias(libs.plugins.google.firebase.crashlytics)
 }
 
 android {
@@ -54,21 +52,15 @@ android {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.code.gson:gson:2.13.2")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.gridlayout)
-    implementation(libs.firebase.common.ktx)
-    implementation(libs.firebase.crashlytics)
-    implementation(libs.firebase.database)
-    implementation(libs.firebase.firestore)
+    implementation(libs.retrofit)
+    implementation(libs.converterGson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- D3Grimoire/app/src/main/java/com/example/d3grimoire/SplashActivity.kt
+++ D3Grimoire/app/src/main/java/com/example/d3grimoire/SplashActivity.kt
@@ -1,46 +1,4 @@
 package com.example.d3grimoire
 
-import API.AuthResponse
-import API.BlizzardAuthInstance
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import com.example.d3grimoire.BuildConfig
-import repository.TokenManager
-
-class SplashActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_screen)
-
-        fetchToken()
-    }
-
-    private fun fetchToken() {
-        val api = BlizzardAuthInstance.create(
-            clientId = BuildConfig.BLIZZARD_CLIENT_ID,
-            clientSecret = BuildConfig.BLIZZARD_CLIENT_SECRET
-        )
-
-        api.getAccessToken().enqueue(object : Callback<AuthResponse> {
-            override fun onResponse(
-                call: Call<AuthResponse>,
-                response: Response<AuthResponse>
-            ) {
-                TokenManager.token = response.body()?.accessToken
-
-                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-                finish()
-            }
-
-            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
-                Log.e("TOKEN", "Failed to fetch token", t)
-            }
-        })
-    }
+class SplashActivity {
 }
