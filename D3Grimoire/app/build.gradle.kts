92d8ee788bfb39d7a0473486dfc530d31d6d6522
DrRon <DoctorRon64@users.noreply.github.com>
2025-12-21 15:29:14 +0100

    [FIXED] not loading after splash

--- D3Grimoire/app/build.gradle.kts
+++ D3Grimoire/app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.firebase.crashlytics)
     implementation(libs.firebase.database)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
